a1fa4dc4a8746d7330d05f020ffc2c9fa11fd2fb
nishikaru <mdsajed001@gamil.com>
2022-12-22 13:46:57 +0600

    added option buttons on chat bubbles and also added a google font

--- src/Comp/ChatBubble/ChatBubble.js
+++ src/Comp/ChatBubble/ChatBubble.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {BsThreeDotsVertical} from 'react-icons/bs'
+import './ChatBubble.css'
 
 const ChatBubble = () => {
     return (
-        <div className='shadow-lg inline-block text-lg font-medium p-4 rounded-md'>
+        <div className='pl-8 gap-3 flex items-center justify-between mainBubble hover:translate-x-[-1em]  transition-all'>
+            <div className='optionIcon transition-all cursor-pointer p-2 hover:shadow-md rounded-full'>
+                <BsThreeDotsVertical/>
+            </div>
+            <div className='shadow-md hover:shadow-lg inline-block text-lg font-medium p-4 rounded-md'>
             <div>
                 This is a text that user posted.
+            </div>
             </div>
         </div>
     );
--- src/Comp/Login/LoginBox.js
+++ src/Comp/Login/LoginBox.js
@@ -6,7 +6,7 @@ const LoginBox = () => {
         <div className='w-[800px] h-[500px] shadow-lg mt-[-4em] rounded-md flex flex-row-reverse'>
             
             <LoginForm/>
-
+            
             <div className='w-1/2 flex items-center justify-center '>
                 <h1 className='text-4xl font-semibold capitalize'>
                     Please<br />log in
