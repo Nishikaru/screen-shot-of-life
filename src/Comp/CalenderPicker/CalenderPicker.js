e8066c044e070585cade1874396cef3b8309225b
nishikaru <mdsajed001@gamil.com>
2022-12-22 10:47:16 +0600

    firebase added

--- src/Comp/CalenderPicker/CalenderPicker.js
+++ src/Comp/CalenderPicker/CalenderPicker.js
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 const CalendarPicker = () => {
   const [value, onChange] = useState(new Date());
-
+  useEffect(()=>{
+    console.log(value);
+  },[])
+  const datePicker = () => {
+    console.log(value);
+  }
   return (
     <div className='shadow-lg text-xl'>
-      <Calendar className='rounded-md' onChange={onChange} value={value} />
+      <Calendar className='rounded-md' onClickDay={datePicker} onChange={onChange} value={value} />
     </div>
   );
 }
