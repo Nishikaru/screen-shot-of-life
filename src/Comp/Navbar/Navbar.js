7d0d4fc93ae120a1f72496886e644e02d1d1bc08
nishikaru <mdsajed001@gamil.com>
2022-12-28 21:43:44 +0600

    menu button is working in mobile view

--- src/Comp/Navbar/Navbar.js
+++ src/Comp/Navbar/Navbar.js
@@ -1,14 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../UserContext/UserContext';
 import PhoneMenu from './PhoneMenu/PhoneMenu';
+import {RiMenu4Line} from 'react-icons/ri'
 
 const Navbar = () => {
     const {theme,user,LogOut} = useContext(AuthContext);
+    const [menu,setMenu] = useState(false)
     // console.log(user.uid)
     return (
         <div>
-            <nav className={`flex justify-between items-center md:px-80 py-3 transition-all shadow-lg bg-skin-${theme.MainBg}`}>
+            <nav className={`flex justify-between items-center px-4 md:px-80 py-3 transition-all shadow-lg bg-skin-${theme.MainBg}`}>
+                <div className='md:hidden'></div>
                 <div>
                 <Link className='capitalize text-xl font-medium hover:shadow-lg rounded-md p-2 transition-all text-shadow-lg' to='/'>screen shot of life</Link>
                 </div>
@@ -18,10 +21,11 @@ const Navbar = () => {
                 <Link className='hover:shadow-lg rounded-md p-2 transition-all' to='/memories'>Memores</Link>
                 <Link className='hover:shadow-lg rounded-md p-2 transition-all'>Plus<sup>+</sup></Link>
                 </div>
-                <div className='md:hidden relative left-0 top-0'>
-                <PhoneMenu/>
+                <div onClick={()=>setMenu(!menu)} className='md:hidden'>
+                    <RiMenu4Line/>
+                <PhoneMenu menu={menu} />
                 </div>
-                <div className='hidden'>
+                <div className='hidden md:inline-block'>
                     {
                         user?.uid?<button onClick={LogOut} className='hover:shadow-lg rounded-md p-2 transition-all'>Log out</button>:
                         <Link className='hover:shadow-lg rounded-md p-2 transition-all' to='/login'>Log in</Link>
--- src/Comp/Navbar/PhoneMenu/PhoneMenu.js
+++ src/Comp/Navbar/PhoneMenu/PhoneMenu.js
@@ -2,14 +2,20 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../UserContext/UserContext';
 
-const PhoneMenu = () => {
-    const {theme} = useContext(AuthContext);
+const PhoneMenu = ({menu}) => {
+    const {theme,LogOut,user} = useContext(AuthContext);
     return (
         <div>
-            <div className={`absolute top-14 bg-skin-${theme.MainBg} flex flex-col gap-3 md:hidden`}>
+            <div className={`${menu?'flex':'hidden'} w-screen h-screen absolute top-0 left-0 bg-black/40`}></div>
+            <div className={`${menu?'inline-block':'hidden'} bg-white absolute top-14 right-3 bg-skin-${theme.MainBg} flex flex-col gap-3 md:hidden shadow-lg rounded-md p-4`}>
                 <Link className='hover:shadow-lg rounded-md p-2 transition-all' to='/'>Home</Link>
                 <Link className='hover:shadow-lg rounded-md p-2 transition-all' to='/memories'>Memores</Link>
                 <Link className='hover:shadow-lg rounded-md p-2 transition-all'>Plus<sup>+</sup></Link>
+                {
+                        user?.uid?<button onClick={LogOut} className='hover:shadow-lg rounded-md p-2 transition-all'>Log out</button>:
+                        <Link className='hover:shadow-lg rounded-md p-2 transition-all' to='/login'>Log in</Link>
+                        
+                    }
                 </div>
         </div>
     );
